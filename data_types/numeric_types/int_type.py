fd10e9e5c9116c745957dc8ae58d8c5784698560
Petar Netev <pesho.netev@gmail.com>
2022-11-22 20:24:15 +0200

    Update in meet activity

---
+++ data_types/numeric_types/int_type.py
@@ -0,0 +1,15 @@
+import math
+
+number_one = 15
+print(bin(number_one))
+print(hex(number_one))
+print(oct(number_one))
+
+print(int(0o17))
+print(int("88"))
+
+# The bellow will throw ValueError
+# print(int("Pesho"))
+
+long_number_display = 100_000_000_000
+print(long_number_display)
