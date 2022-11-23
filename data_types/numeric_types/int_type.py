b3360f91f133dffc4eb6f352150e58eb6d550f9c
Petar Netev <pesho.netev@gmail.com>
2022-11-23 17:20:12 +0200

    Add simple match operations between number types

--- data_types/numeric_types/int_type.py
+++ data_types/numeric_types/int_type.py
@@ -1,4 +1,5 @@
 import math
+from decimal import Decimal
 
 number_one = 15
 print(bin(number_one))
@@ -8,8 +9,15 @@ print(oct(number_one))
 print(int(0o17))
 print(int("88"))
 
+float()
+Decimal()
 # The bellow will throw ValueError
 # print(int("Pesho"))
 
 long_number_display = 100_000_000_000
 print(long_number_display)
+
+
+a = 5
+b = 3.5
+c = a + b
