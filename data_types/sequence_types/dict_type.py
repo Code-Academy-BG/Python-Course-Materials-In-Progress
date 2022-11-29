59f5d2ad212b1c7a3a1dfef5e9e5f7459ac5a3d1
Petar Netev <pesho.netev@gmail.com>
2022-11-29 20:26:04 +0200

    Update in meet activity

---
+++ data_types/sequence_types/dict_type.py
@@ -0,0 +1,48 @@
+fruits_colors = {
+    "green_apple": "green",
+    "banana": "yellow",
+    "peach": "orange",
+}
+
+print(fruits_colors["green_apple"])
+print(fruits_colors.get(0, "NOT FOUND"))
+
+fruits_colors["orange"] = "ohra"
+print(fruits_colors)
+
+print(fruits_colors.setdefault("orange", "pink"))
+print(fruits_colors)
+
+fruits_colors["banana"] = "green"
+print(fruits_colors)
+
+# fruits_colors.update(banana="yellow")
+# fruits_colors.update({"banana": "yellow"})
+# print(fruits_colors)
+
+print(len(fruits_colors))
+
+# Iterate by using iteration on keys and Key look up
+for key in fruits_colors:
+    print(key, fruits_colors[key])
+
+# print(fruits_colors.keys())
+# print(fruits_colors.values())
+# print(fruits_colors.items())
+#
+# for key in fruits_colors.keys():
+#     print(key)
+#
+# for value in fruits_colors.values():
+#     print(value)
+#
+# for key, value in fruits_colors.items():
+#     print(key, value)
+
+orange = fruits_colors.pop("orange", "NO SUCH FRUIT")
+print(orange)
+print(fruits_colors)
+
+popped_item = fruits_colors.popitem()
+
+del fruits_colors
---
+++ data_types/sequence_types/tuple_type.py
@@ -0,0 +1,17 @@
+names = ("georgi", "tosho", "pesho")
+print(names[0])
+print(id(names))
+# names = names + ("misho", "ivan")
+print(id(names))
+print(names)
+print(names.count("georgi"))
+
+strange_tuple = ("Pesho", 32, [])
+strange_tuple[-1].append(1)
+print(strange_tuple, len(strange_tuple))
+
+# This won't work
+# strange_tuple[1] = 44
+# print(strange_tuple)
+
+tuple_of_single_element = (1,)
